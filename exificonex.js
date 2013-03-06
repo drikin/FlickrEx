36f7639c4fc73cc4352f9a762630bd424eb70461
Kohichi Aoki <drikin@gmail.com>
2013-03-06 15:37:03 -0800

    added js files again

---
+++ exificonex.js
@@ -0,0 +1,167 @@
+var __extends = this.__extends || function (d, b) {
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var com;
+(function (com) {
+    (function (teruhisa) {
+        (function (ExifIconEx) {
+            var flickrex = new com.drikin.FlickrEx.Base();
+            var exif_jquery_selector = 'img';
+            if(window.FLICKREX_EXIF_JQUERY_SELECTOR !== undefined) {
+                exif_jquery_selector = window.FLICKREX_EXIF_JQUERY_SELECTOR;
+                delete window.FLICKREX_EXIF_JQUERY_SELECTOR;
+            }
+            var partial_map = {
+                ".": "dot"
+            };
+            var container_class = 'flickr-exif-icon';
+            var ExifProperty = (function () {
+                function ExifProperty(type, exif) {
+                    this.type = type;
+                    this.exif = exif;
+                    this.classPrefix = container_class + '-';
+                }
+                ExifProperty.prototype.getValue = function () {
+                    return this.exif ? this.exif.raw._content : "";
+                };
+                ExifProperty.prototype.getValueBlockClass = function (partial) {
+                    return 'text-' + (partial_map[partial] || partial) + ' ' + this.classPrefix + 'text';
+                };
+                ExifProperty.prototype.getBlock = function (tag, blockName, content) {
+                    return '<' + tag + ' class="' + this.classPrefix + blockName + '">' + content + '</' + tag + '>';
+                };
+                ExifProperty.prototype.getHtmlBlocks = function () {
+                    var val = this.getValue(), partial, blocks = "";
+                    if(val) {
+                        for(var bi = 0; bi < val.length; bi++) {
+                            partial = val[bi];
+                            blocks += this.getBlock('span', this.getValueBlockClass(partial), partial);
+                        }
+                    } else {
+                        blocks = this.getBlock('span', 'empty', 'empty');
+                    }
+                    return blocks;
+                };
+                ExifProperty.prototype.getHtml = function () {
+                    return this.getBlock('div', this.type, this.getHtmlBlocks());
+                };
+                return ExifProperty;
+            })();
+            ExifIconEx.ExifProperty = ExifProperty;            
+            var ExifAperture = (function (_super) {
+                __extends(ExifAperture, _super);
+                function ExifAperture(type, exif) {
+                                _super.call(this, type, exif);
+                }
+                ExifAperture.prototype.getValue = function () {
+                    return parseFloat(_super.prototype.getValue.call(this)) + "";
+                };
+                ExifAperture.prototype.getHtmlBlocks = function () {
+                    var blocks = _super.prototype.getHtmlBlocks.call(this);
+                    return this.getBlock('span', 'label', 'F') + blocks;
+                };
+                return ExifAperture;
+            })(ExifProperty);
+            ExifIconEx.ExifAperture = ExifAperture;            
+            var ExifExposure = (function (_super) {
+                __extends(ExifExposure, _super);
+                function ExifExposure(type, exif) {
+                                _super.call(this, type, exif);
+                    this.fraction = false;
+                }
+                ExifExposure.prototype.getValue = function () {
+                    var val = _super.prototype.getValue.call(this);
+                    if(val && val.indexOf('1/') === 0) {
+                        val = val.substring(2);
+                        this.fraction = true;
+                    }
+                    return val;
+                };
+                ExifExposure.prototype.getHtmlBlocks = function () {
+                    var blocks = _super.prototype.getHtmlBlocks.call(this);
+                    var frac = this.fraction ? this.getBlock('span', 'text-frac', '1/') : "";
+                    return frac + blocks + this.getBlock('span', 'label', 'S');
+                };
+                return ExifExposure;
+            })(ExifProperty);
+            ExifIconEx.ExifExposure = ExifExposure;            
+            var ExifISO = (function (_super) {
+                __extends(ExifISO, _super);
+                function ExifISO(type, exif) {
+                                _super.call(this, type, exif);
+                    this.highiso = false;
+                }
+                ExifISO.prototype.getValue = function () {
+                    var val = _super.prototype.getValue.call(this);
+                    if(val && val.length > 4) {
+                        val = val.substring(0, val.length - 2);
+                        this.highiso = true;
+                    }
+                    return val;
+                };
+                ExifISO.prototype.getHtmlBlocks = function () {
+                    var blocks = _super.prototype.getHtmlBlocks.call(this);
+                    return this.getBlock('span', 'label', 'ISO') + blocks + (this.highiso ? this.getBlock('span', 'text-high', '00') : "");
+                };
+                return ExifISO;
+            })(ExifProperty);
+            ExifIconEx.ExifISO = ExifISO;            
+            var exif_block_tags = [
+                'aperture', 
+                'exposure', 
+                'iso speed'
+            ];
+            var exif_block_handlers = {
+                'aperture': ExifAperture,
+                'exposure': ExifExposure,
+                'iso speed': ExifISO
+            };
+            $(document).ready(function () {
+                var flickr_imgs = flickrex.getAllFlickrImageObjects(exif_jquery_selector);
+                for(var i = 0, l = flickr_imgs.length; i < l; i++) {
+                    (function () {
+                        var flickr_img = flickr_imgs[i];
+                        flickrex.getExif(flickr_imgs[i].id, function (exif_data) {
+                            generateIcon(flickr_img, exif_data);
+                        });
+                    })();
+                }
+            });
+            function generateIcon(flickr_img, exif_data) {
+                var exif = exif_data.photo.exif;
+                var exif_string = "";
+                var exif_blocks = [];
+                for(var ei = 0, el = exif.length; ei < el; ei++) {
+                    var exifObj = exif[ei];
+                    var exifTag = exifObj.tag.toLowerCase();
+                    var exifLabel = exifObj.label.toLowerCase();
+                    var pos = exif_block_tags.indexOf(exifLabel);
+                    if(pos !== -1) {
+                        var exifProperty = new exif_block_handlers[exifLabel](exifTag, exifObj);
+                        exif_blocks[pos] = exifProperty.getHtml();
+                    }
+                }
+                if(exif_blocks.length > 0) {
+                    for(var bi = 0; bi < exif_block_tags.length; bi++) {
+                        var exif_block = exif_blocks[bi];
+                        if(exif_block) {
+                            exif_string += exif_block;
+                        } else {
+                            var exifProperty = new ExifProperty(exifTag, null);
+                            exif_string += exifProperty.getHtml();
+                        }
+                    }
+                }
+                if(exif_string) {
+                    var p = $("<div class='" + container_class + "' style='width:" + flickr_img.node.width + "px'><div class='" + container_class + "-block'>" + exif_string + "</div></div>");
+                    $(flickr_img.node).after(p);
+                }
+            }
+            ;
+        })(teruhisa.ExifIconEx || (teruhisa.ExifIconEx = {}));
+        var ExifIconEx = teruhisa.ExifIconEx;
+    })(com.teruhisa || (com.teruhisa = {}));
+    var teruhisa = com.teruhisa;
+})(com || (com = {}));
---
+++ flickrex.js
@@ -0,0 +1,96 @@
+var com;
+(function (com) {
+    (function (drikin) {
+        (function (FlickrEx) {
+            var Base = (function () {
+                function Base() {
+                    this.api_key = "18c9f79a96fd34c3b3f16a93fb0a5d3c";
+                    this.api_type = "json&nojsoncallback=1";
+                    this.base_url = "http://api.flickr.com/services/rest/?";
+                    if(window.FLICKREX_API_KEY !== undefined) {
+                        this.api_key = window.FLICKREX_API_KEY;
+                        delete window.FLICKREX_API_KEY;
+                    }
+                    this.base_url = this.appendURLParams({
+                        api_key: this.api_key
+                    });
+                    this.base_url = this.appendURLParams({
+                        format: this.api_type
+                    });
+                }
+                Base.prototype.appendURLParams = function (params) {
+                    var return_url = this.base_url;
+                    for(var key in params) {
+                        return_url += "&" + key + "=" + params[key];
+                    }
+                    return return_url;
+                };
+                Base.prototype.getJsonResult = function (request_url, callback) {
+                    $.ajax(request_url, {
+                        success: function (data) {
+                            callback(data);
+                        },
+                        dataType: 'json'
+                    });
+                };
+                Base.prototype.parseFlickrImageURL = function (node) {
+                    var url_string = $(node).attr('src');
+                    var url_elems = url_string.split('/');
+                    var farm_id = url_elems[2].split('.').slice(0, 1)[0].replace('farm', '');
+                    var server_id = url_elems[3];
+                    var url_last_components = url_elems[4].split('_');
+                    var id = url_last_components.slice(0, 1)[0];
+                    var secret = url_last_components.slice(1, 2)[0];
+                    var secret = url_last_components.slice(1, 2)[0];
+                    var size_info = url_last_components.slice(2, 3);
+                    if(size_info.length) {
+                        var size = size_info.length && size_info[0].split('.').slice(0, 1)[0];
+                    } else {
+                        var size = '';
+                    }
+                    var file_ext = url_elems.slice(-1)[0].split('.').slice(-1)[0];
+                    var obj = {
+                        node: node,
+                        url_string: url_string,
+                        farm_id: farm_id,
+                        server_id: server_id,
+                        id: id,
+                        secret: secret,
+                        size: size,
+                        file_ext: file_ext
+                    };
+                    return obj;
+                };
+                Base.prototype.getAllFlickrImageObjects = function (jquery_selector) {
+                    if (typeof jquery_selector === "undefined") { jquery_selector = 'img'; }
+                    var imgs = $(jquery_selector).filter(function (idx) {
+                        var src_str = $(this).attr('src');
+                        return src_str && src_str.match(/static.?flickr.com/);
+                    });
+                    var objs = [];
+                    for(var i = 0, l = imgs.length; i < l; i++) {
+                        objs.push(this.parseFlickrImageURL(imgs[i]));
+                    }
+                    return objs;
+                };
+                Base.prototype.getExif = function (photo_id, callback) {
+                    var request_url = this.appendURLParams({
+                        method: "flickr.photos.getExif",
+                        photo_id: photo_id
+                    });
+                    this.getJsonResult(request_url, callback);
+                };
+                Base.prototype.getRecentURL = function (callback) {
+                    var request_url = this.appendURLParams({
+                        method: "flickr.photos.getRecent"
+                    });
+                    this.getJsonResult(request_url, callback);
+                };
+                return Base;
+            })();
+            FlickrEx.Base = Base;            
+        })(drikin.FlickrEx || (drikin.FlickrEx = {}));
+        var FlickrEx = drikin.FlickrEx;
+    })(com.drikin || (com.drikin = {}));
+    var drikin = com.drikin;
+})(com || (com = {}));
